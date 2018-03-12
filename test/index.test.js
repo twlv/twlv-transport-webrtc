3f2d65c95582efc8078db326f4b0879966935026
Ganesha <reekoheek@gmail.com>
2018-03-13 01:09:32 +0700

    Release 0.1.0

--- test/index.test.js
+++ test/index.test.js
@@ -1,20 +1,23 @@
 const { Node } = require('@twlv/core');
-const MemoryFinder = require('@twlv/core/finders/memory');
+const { MemoryFinder } = require('@twlv/core/finders/memory');
 const { MemoryDialer, MemoryListener } = require('@twlv/core/transports/memory');
 const { WebRTCDialer, WebRTCListener } = require('..');
 const assert = require('assert');
-const wrtc = require('electron-webrtc')(); // TODO: electron wrtc leak?
+// const wrtc = require('electron-webrtc')(); // TODO: electron wrtc leak?
+const wrtc = require('wrtc'); // TODO: electron wrtc leak?
 
-describe('WebRTC Transport', () => {
+describe.only('WebRTC Transport', () => {
   before(() => process.on('unhandledRejection', err => console.error('Unhandled', err)));
   after(() => process.removeAllListeners('unhandledRejection'));
 
   it('send to other peer', async () => {
-    let gw = new Node();
+    let gw1 = new Node();
+    let gw2 = new Node();
     let node1 = new Node();
     let node2 = new Node();
 
-    gw.addFinder(new MemoryFinder());
+    gw1.addFinder(new MemoryFinder());
+    gw2.addFinder(new MemoryFinder());
     node1.addFinder(new MemoryFinder());
     node2.addFinder(new MemoryFinder());
 
@@ -24,17 +27,21 @@ describe('WebRTC Transport', () => {
     node1.addListener(new WebRTCListener({ wrtc }));
     node2.addListener(new WebRTCListener({ wrtc }));
 
-    gw.addListener(new MemoryListener());
+    gw1.addListener(new MemoryListener());
+    gw2.addListener(new MemoryListener());
     node1.addDialer(new MemoryDialer());
     node2.addDialer(new MemoryDialer());
 
     try {
-      await gw.start();
+      await gw1.start();
+      await gw2.start();
       await node1.start();
       await node2.start();
 
-      await node1.connect(`memory:${gw.identity.address}`);
-      await node2.connect(`memory:${gw.identity.address}`);
+      await node1.connect(`memory:${gw1.identity.address}`);
+      await node2.connect(`memory:${gw1.identity.address}`);
+      await node1.connect(`memory:${gw2.identity.address}`);
+      await node2.connect(`memory:${gw2.identity.address}`);
 
       node2.on('message', message => {
         if (message.command.startsWith('transport:webrtc')) {
@@ -51,7 +58,8 @@ describe('WebRTC Transport', () => {
         payload: 'bar',
       });
     } finally {
-      await gw.stop();
+      await gw1.stop();
+      await gw2.stop();
       await node1.stop();
       await node2.stop();
     }
