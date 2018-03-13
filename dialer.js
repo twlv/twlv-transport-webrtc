3567ae42370c7f329f5c068b8d554f54e2d80859
Ganesha <reekoheek@gmail.com>
2018-03-13 14:48:36 +0700

    Implement dial timeout default to 30s

--- dialer.js
+++ dialer.js
@@ -1,14 +1,22 @@
 const Socket = require('simple-peer');
 class WebRTCDialer {
-  constructor ({ wrtc } = {}) {
+  constructor ({ wrtc, timeout = 30000 } = {}) {
     this.proto = 'wrtc';
+
+    this.timeout = timeout;
     this.wrtc = wrtc;
   }
 
   dial (url, node) {
     let address = url.split(':').pop();
+
     return new Promise((resolve, reject) => {
       let socket = new Socket({ initiator: true, wrtc: this.wrtc, trickle: true });
+
+      let dialTimeout = setTimeout(() => {
+        socket.destroy();
+        reject(new Error('WebRTC Dial timeout'));
+      }, this.timeout);
 
       let onMessage = message => {
         if (message.command !== 'transport:webrtc:signal') {
@@ -41,6 +49,7 @@ class WebRTCDialer {
       socket.on('connect', () => {
         node.removeListener('message', onMessage);
         socket.removeAllListeners();
+        clearTimeout(dialTimeout);
         resolve(socket);
       });
     });
